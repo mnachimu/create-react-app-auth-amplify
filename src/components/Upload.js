903c1483ef474cbe99190be9b86fe3c0acc67cc8
Mugunth <mnachimu@asu.edu>
2022-08-09 01:44:42 -0700

    Try new

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,38 +1,70 @@
 import React, { useRef } from "react";
 import S3 from "react-aws-s3";
+var AWS = require('aws-sdk');
+var fs =  require('fs');
 
 function Upload() {
+  var s3 = new AWS.S3();
   const fileInput = useRef();
   const textInput = useRef();
+
   const handleClick = (event) => {
-  console.log("something");
-    event.preventDefault();
-    let file = fileInput.current.files[0];
-    let newFileName = fileInput.current.files[0].name.replace(/\..+$/, "");
-    console.log(process.env.USER_BRANCH);
-    console.log(process.env.AK);
-    console.log(process.env.SC);
-    const config = {
-      bucketName: 'testbucket-762673298322',
-      dirName: process.env.REACT_APP_DIR_NAME /* optional */,
-      region: 'us-west-1',
-      accessKeyId: process.env.AK,
-      secretAccessKey: process.env.SC,
-    };
+//  console.log("something");
+//    event.preventDefault();
+//    let file = fileInput.current.files[0];
+//    let newFileName = fileInput.current.files[0].name.replace(/\..+$/, "");
+//    console.log(process.env.USER_BRANCH);
+//    console.log(process.env.AK);
+//    console.log(process.env.SC);
+//    const config = {
+//      bucketName: 'testbucket-762673298322',
+//      dirName: process.env.REACT_APP_DIR_NAME /* optional */,
+//      region: 'us-west-1',
+//      accessKeyId: process.env.AK,
+//      secretAccessKey: process.env.SC,
+//    };
+//    const handleSubmit = (event) => {
+//        // Initialize the Amazon Cognito credentials provider
+//        const s3 = new S3Client({
+//          region: REGION,
+//          credentials: fromCognitoIdentityPool({
+//            client: new CognitoIdentityClient({ region: REGION }),
+//            identityPoolId: "IDENTITY_POOL_ID", // IDENTITY_POOL_ID
+//          }),
+//        });
+    var myBucket = 'testbucket-347985515607';
+
+    var myKey = 'text';
+    console.log("about to read the file and upload to s3");
+    console.log();
+    fs.readFile(file, function (err, data) {
+      if (err) { throw err; }
+        params = {Bucket: myBucket, Key: myKey, Body: data };
+        s3.putObject(params, function(err, data) {
+         if (err) {
+            alert(err);
+            console.log(err)
+         } else {
+            console.log("Successfully uploaded data to myBucket/myKey");
+         }
+        });
+      });
+    }
+
     console.log("printing config");
     console.log(config);
     const ReactS3Client = new S3(config);
-    ReactS3Client.uploadFile(file, newFileName).then((data) => {
-      console.log(data);
-      if (data.status === 204) {
-        console.log("success");
-      } else {
-        console.log("fail");
-      }
-    }).catch((err)=>{
-        alert(err);
-        console.log(err);
-    });
+//    ReactS3Client.uploadFile(file, newFileName).then((data) => {
+//      console.log(data);
+//      if (data.status === 204) {
+//        console.log("success");
+//      } else {
+//        console.log("fail");
+//      }
+//    }).catch((err)=>{
+//        alert(err);
+//        console.log(err);
+//    });
     console.log(newFileName);
     console.log(textInput);
     console.log(config.bucketName);
